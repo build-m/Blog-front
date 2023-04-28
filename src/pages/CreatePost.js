18dd604bc658b760ba9c8767fda59cd682afc474
surafel <78304246+build-m@users.noreply.github.com>
2023-04-28 13:22:04 -0700

    Update CreatePost.js

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -14,6 +14,7 @@ export default function CreatePost() {
    const [images, setImages] = useState('')
    
    const baseurl = 'https://uptight-plum-pleat.cyclic.app'
+   
 
    const modules = { 
     toolbar:[
